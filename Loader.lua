2fe5072d0fcbc1e3690551ad9b23ef9a27773511
Meowy <117300772+GodYT88@users.noreply.github.com>
2025-07-30 22:06:07 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,7 +5,7 @@ local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 local PlaceId = game.PlaceId
 local Games = {
-    [11455876520] = "https://raw.githubusercontent.com/GodYT88/Allusion/refs/heads/main/Games/Trade-Strength-Simulator",
+    [11455876520] = "https://api.luarmor.net/files/v3/loaders/97bcf334758e81ee33dc522cf41ccadd.lua",
 }
 local FoundGame = Games[PlaceId] or false
 if FoundGame then
