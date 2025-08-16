6411b43f37ef013d2584b4b4705f0d7c93994529
Meowy <117300772+GodYT88@users.noreply.github.com>
2025-08-16 14:59:51 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -9,11 +9,11 @@ local Games = {
     [11455876520] = "https://api.luarmor.net/files/v3/loaders/97bcf334758e81ee33dc522cf41ccadd.lua",
 }
 local FoundGame = Games[PlaceId] or false
-local FoundGame2 = loadstring(game:HttpGet(Allusion_DarkLoader))()
+local FoundGame2 = Allusion_DarkLoader ~= "" and loadstring(game:HttpGet(Allusion_DarkLoader))()
 if FoundGame then
     loadstring(game:HttpGet(FoundGame))()
 elseif FoundGame2 then
-    loadstring(game:HttpGet(FoundGame2))()
+    FoundGame2()
 else
     Player:Kick("Allusion : Game Not Support")
 end
